4e4d4db278fb447d98add8868253c33c5e559b27
soundaryapurushothaman <143268635+soundaryapurushothaman@users.noreply.github.com>
2025-06-17 16:12:37 +0530

    Create binaryTree.java

---
+++ binaryTree.java
@@ -0,0 +1,124 @@
+class Node
+{
+	int data;
+	Node left;
+	Node right;
+
+	Node(int data)
+	{
+		this.data=data;
+		left=null;
+		right=null;
+	}
+}
+
+
+class BinaryTree
+{
+	Node root;
+
+	Node insert(Node root, int key)
+	{
+		if(root==null)
+			return new Node(key);
+
+		if(root.left==null)
+			root.left=new Node(key);
+		else if(root.right==null)
+			root.right=new Node(key);
+		else
+			root.left=insert(root.left,key);
+
+		return root;
+
+	}
+
+	void inorderDisp(Node root)
+	{
+		if(root!=null)
+		{
+			inorderDisp(root.left);
+			System.out.print(root.data+" ");
+			inorderDisp(root.right);
+		}
+
+	}
+
+	void preorderDisp(Node root)
+	{
+		if(root!=null)
+		{
+			System.out.print(root.data+" ");
+			preorderDisp(root.left);
+			preorderDisp(root.right);
+		}
+
+	}
+
+
+	void postorderDisp(Node root)
+	{
+		if(root!=null)
+		{
+			postorderDisp(root.left);
+			postorderDisp(root.right);
+			System.out.print(root.data+" ");
+		}
+	}
+
+	Node delete(Node root, int key) {
+		if (root == null)
+			return null;
+
+
+		if (root.data == key) {
+
+			if (root.left == null && root.right == null) {
+				return null;
+			}
+
+			if (root.left == null)
+				return root.right;
+			if (root.right == null)
+				return root.left;
+
+
+			Node temp = findMin(root.right);
+			root.data = temp.data;
+			root.right = delete(root.right, temp.data);
+			return root;
+		}
+
+		root.left = delete(root.left, key);
+		root.right = delete(root.right, key);
+		return root;
+	}
+
+	Node findMin(Node root) {
+		while (root.left != null) {
+			root = root.left;
+		}
+		return root;
+	}
+}
+class Main {
+	public static void main(String[] args)
+	{
+		BinaryTree t=new BinaryTree();
+
+		t.root = t.insert(t.root, 50);
+		t.root = t.insert(t.root, 20);
+		t.root = t.insert(t.root, 60);
+
+		t.postorderDisp(t.root);
+
+		t.root=t.delete(t.root,20);
+		t.inorderDisp(t.root);
+	}
+
+}
+
+
+
+
+
